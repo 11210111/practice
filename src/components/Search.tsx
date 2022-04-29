db63d33a36d9eea2ac49f77d63b39b734373ed8b
11210111 <jagarmj@gmail.com>
2022-04-29 16:09:58 +0900

    feat 검색기능 구현

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchInput from './SearchInput';
 import SearchList from './SearchList';
 
+export interface searchKeyword {
+  id: number;
+  keyword: string;
+}
+
 const Search: React.FC = () => {
+  const [keyword, setKeyword] = useState<string>('');
+  const [keywords, setKeywords] = useState<searchKeyword[]>([]);
+
+  const handleAddKeyword = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (keyword) {
+      const mapKeywords = keywords.map((data) => data.keyword);
+      if (!mapKeywords.includes(keyword)) {
+        setKeywords([{ id: Date.now(), keyword }, ...keywords]);
+      }
+    }
+  };
+
   return (
     <article className='search'>
-      <SearchInput />
+      <SearchInput
+        keyword={keyword}
+        setKeyword={setKeyword}
+        onAddKeyword={handleAddKeyword}
+      />
       <SearchList />
     </article>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,7 +3,17 @@ import Button from './Button';
 
 import { BsSearch } from 'react-icons/bs';
 
-const SearchInput: React.FC = () => {
+interface Props {
+  keyword: string;
+  setKeyword: React.Dispatch<React.SetStateAction<string>>;
+  onAddKeyword: (e: React.FormEvent) => void;
+}
+
+const SearchInput: React.FC<Props> = ({
+  keyword,
+  setKeyword,
+  onAddKeyword,
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -12,13 +22,15 @@ const SearchInput: React.FC = () => {
     }
   }, []);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
-
   return (
-    <form className='search-form' onSubmit={handleSubmit}>
-      <input type='text' className='search-input' ref={inputRef} />
+    <form className='search-form' onSubmit={(e) => onAddKeyword(e)}>
+      <input
+        type='text'
+        className='search-input'
+        ref={inputRef}
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <Button text={<BsSearch />} type='search' />
     </form>
   );
